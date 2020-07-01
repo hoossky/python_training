426a45372af46c396e48d9645412e144d7fe1ad9
이호섭 <62980969+hoossky@users.noreply.github.com>
2020-07-01 10:22:36 +0900

    "10:22"

--- basic/mining.py
+++ basic/mining.py
@@ -9,6 +9,7 @@ import re
 
 import matplotlib.pyplot as plt
 
+
 @dataclass
 class Model:
     context: str
@@ -43,11 +44,38 @@ class Service:
         self.freqtxt = []
 
     def extract_token(self, payload):
-        print('>> text 문서에서 token 추출')
+        print('1. corpus 문서에서 token 추출')
         filename = payload.context + payload.fname
         with open(filename, 'r', encoding='utf-8') as f:
             self.texts = f.read()
-        print(f'{self.texts[:300]}')
+        print(f'1단계 결과물 : {self.texts[:300]}')
+
+    def extract_hanguel(self):
+        print('2. corpus 문서에서 한글 추출')
+        texts = self.texts.replace('\n', ' ')
+        tokenizer = re.compile(r'[^ㄱ-힣]')
+        # [^ ]는 not, ^[]는 시작
+        self.texts = tokenizer.sub('', texts)
+        # 한글이 아닌 것은 '' 처리해서 한글만 남겨라
+        print(f'2단계 결과물 : {self.texts[:300]}')
+
+    def conversion_token(self):
+        print('3. 한글 token 변환')
+
+    def compound_noun(self):
+        print('4. 복합 명사화')
+
+    def extract_stopword(self):
+        print('5. 노이즈 코퍼스에서 토큰 추출')
+
+    def filtering_text_with_stopword(self):
+        print('6. 노이즈 필터링 후 시그널 추출')
+
+    def frequent_text(self):
+        print('7. 시그널 중에 사용빈도 정렬')
+
+    def wordcloud(self):
+        print('8. 시각화')
 
 
 class Controller:
@@ -57,10 +85,23 @@ class Controller:
     def download_dictionary(self):
         nltk.download('all')
 
+    def data_analysis(self):
+        entity = Entity()
+        service = Service()
+        service.extract_token()
+        service.extract_hanguel()
+        service.conversion_token()
+        service.compound_noun()
+        service.extract_stopword()
+        service.filtering_text_with_stopword()
+        service.frequent_text()
+        service.wordcloud()
+
 
 def print_menu():
     print('0. Exit')
     print('1. 사전 다운로드')
+    print('2. 데이터 분석')
     return input('메뉴 선택\n')
 
 
@@ -69,5 +110,9 @@ while 1:
     menu = print_menu()
     if menu == '1':
         app.download_dictionary()
+
+    if menu == '2':
+        app.data_analysis()
+
     elif menu == '0':
         break
